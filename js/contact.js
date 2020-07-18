424de6273eff353b7739df0d51f05241ffc86b0b
Ardd <arddluma@gmail.com>
2020-07-18 20:08:25 +0200

    Contact Form

--- js/contact.js
+++ js/contact.js
@@ -54,31 +54,31 @@ $(document).ready(function(){
                     minlength: "thats all? really?"
                 }
             },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
-        })
+        //     submitHandler: function(form) {
+        //         $(form).ajaxSubmit({
+        //             type:"POST",
+        //             data: $(form).serialize(),
+        //             url:"https://kwes.io/api/foreign/forms/OLNmBfzjnhdJUhk39UoJ",
+        //             success: function() {
+        //                 $('#contactForm :input').attr('disabled', 'disabled');
+        //                 $('#contactForm').fadeTo( "slow", 1, function() {
+        //                     $(this).find(':input').attr('disabled', 'disabled');
+        //                     $(this).find('label').css('cursor','default');
+        //                     $('#success').fadeIn()
+        //                     $('.modal').modal('hide');
+		//                 	$('#success').modal('show');
+        //                 })
+        //             },
+        //             error: function() {
+        //                 $('#contactForm').fadeTo( "slow", 1, function() {
+        //                     $('#error').fadeIn()
+        //                     $('.modal').modal('hide');
+		//                 	$('#error').modal('show');
+        //                 })
+        //             }
+        //         })
+        //     }
+        // })
     })
         
  })(jQuery)
